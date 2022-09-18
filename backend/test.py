3c77bc2df3c7a997f17c1b501ced831384a4341a
Khalid Zabalawi <58151955+HikaruSadashi@users.noreply.github.com>
2022-09-17 22:48:45 -0400

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -2,25 +2,26 @@ from urllib import request
 import psycopg2
 import os
 
-url = 'postgresql://moon:FGO0xNTn_cd85kmo2HQADQ@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Daltima-5043'
+url = 'postgresql://moon:oFvnSKk11YxC_Oeh627BjA@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Daltima-5043'
 conn = psycopg2.connect(url)
 # conn = psycopg2.connect(os.environ['DATABASE_URL'])
 
+'''
 #SQL database structure: userID, timePosted, lat, long
-""" MAKE TABLE (only once)
 with conn.cursor() as cur:
     cur.execute("CREATE TABLE requestList (userID varchar, timePosted date, lat float, long float);")
-
-"""
+conn.commit()
+'''
 
 # Call this function in the request route
 def postRequest(userID, lat, long):
-    date = str(date.today())
+    #date = str(date.today())
+    date = str(51122)
 
     with conn.cursor() as cur:
         cur.execute("""
-        INSERT INTO requestList (a_string, a_date, a_float, a_float)
-        VALUES (%s, %s, %f, %f);
+        INSERT INTO requestList (userID, timePosted, lat, long)
+        VALUES (%s, %s, %s, %s);
         """, 
         (userID, date, lat, long))
     conn.commit()
@@ -29,9 +30,16 @@ def postRequest(userID, lat, long):
 
 def getRequest(userID):
     with conn.cursor() as cur:
-        query = cur.execute(f""" SELECT * FROM requestList WHERE userid = {userID} """)
+        query = cur.execute(""" SELECT * FROM requestList WHERE userid = %s """, userID)
     conn.commit()
 
 
     #returns table as json object
     return query
+
+#test first function
+userID = 'test'
+lat = 448473
+long = 394857
+
+postRequest(userID, lat, long)
